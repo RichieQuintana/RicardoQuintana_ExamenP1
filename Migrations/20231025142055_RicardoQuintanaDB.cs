10629660f0fadbe62b917563bcdf64e26b66ab33
Ricardo Andres Quintana Maldonado <ricardo.quintana.maldonado@udla.edu.ec>
2023-10-25 09:21:31 -0500

    Migramos, para que no ocurra ningun error al actualizar

---
+++ Migrations/20231025142055_RicardoQuintanaDB.cs
@@ -0,0 +1,32 @@
+﻿using Microsoft.EntityFrameworkCore.Migrations;
+
+#nullable disable
+
+namespace RicardoQuintana_ExamenP1.Migrations
+{
+    public partial class RicardoQuintanaDB : Migration
+    {
+        protected override void Up(MigrationBuilder migrationBuilder)
+        {
+            migrationBuilder.CreateTable(
+                name: "RicardoQuintana_Tabla",
+                columns: table => new
+                {
+                    id = table.Column<int>(type: "int", nullable: false)
+                        .Annotation("SqlServer:Identity", "1, 1"),
+                    name = table.Column<string>(type: "nvarchar(max)", nullable: false),
+                    description = table.Column<string>(type: "nvarchar(max)", nullable: false)
+                },
+                constraints: table =>
+                {
+                    table.PrimaryKey("PK_RicardoQuintana_Tabla", x => x.id);
+                });
+        }
+
+        protected override void Down(MigrationBuilder migrationBuilder)
+        {
+            migrationBuilder.DropTable(
+                name: "RicardoQuintana_Tabla");
+        }
+    }
+}
